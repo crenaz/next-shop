0dce87b5639e02ede99b534e57ad4170661cd19b
crenaz <crenaz@gmail.com>
2022-11-30 17:51:57 -0400

    done for today

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import CategoryCard from '../components/CategoryCard';
 
 const HomePage = () => {
   return (
-    <main>
-      <h1>Shopping Cart</h1>
+    <main className={styles.container}>
+      <div className={styles.small}>
+        <CategoryCard image="/images/cars.jpg" name="Cars" />
+        <CategoryCard image="/images/computers.jpg" name="Computers" />
+        <CategoryCard image="/images/cooking.jpg" name="Cooking" />
+      </div>
+      <div className={styles.large}>
+        <CategoryCard image="/images/cats.jpg" name="Cats" />
+        <CategoryCard image="/images/coffee.jpg" name="Coffee" />
+      </div>
     </main>
   );
 };
+
 export default HomePage;
