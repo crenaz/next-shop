081124101ba26ed69f50ab4bfc0b28ccb88cea5c
crenaz <crenaz@gmail.com>
2022-11-30 16:50:34 -0400

    450

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,8 +3,10 @@ import styles from '../styles/Footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      Copyright <span className={styles.brand}>GamesKart</span>{' '}
+      Copyright <span className={styles.brand}>Crenaz's C-Store</span>{' '}
       {new Date().getFullYear()}
     </footer>
   );
 };
+
+export default Footer
