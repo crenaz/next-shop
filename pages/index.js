4df2e4abe4f852b3b33297afef5cea2d85deb324
crenaz <crenaz@gmail.com>
2022-11-30 22:29:45 -0400

    1029

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 import CategoryCard from '../components/CategoryCard';
 
 const HomePage = () => {
@@ -9,11 +9,11 @@ const HomePage = () => {
       <div className={styles.small}>
         <CategoryCard image="/images/cars.jpg" name="Cars" />
         <CategoryCard image="/images/computers.jpg" name="Computers" />
-        <CategoryCard image="/images/cooking.jpg" name="Cooking" />
+        <CategoryCard image="https://imgur.com/Dm212HS.png" name="Cooking" />
       </div>
       <div className={styles.large}>
-        <CategoryCard image="/images/cats.jpg" name="Cats" />
-        <CategoryCard image="/images/coffee.jpg" name="Coffee" />
+        <CategoryCard image="https://imgur.com/qb6IW1f.png" name="Cats" />
+        <CategoryCard image="https://imgur.com/HsUfuRU.png" name="Coffee" />
       </div>
     </main>
   );
